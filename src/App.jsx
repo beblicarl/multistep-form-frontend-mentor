368ddc2885ff946f29c4d78332c8a7a989c6cfb0
Carl Bebli <beblicarl.cb@gmail.com>
2023-06-20 12:57:12 +0000

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MultistepForm from "./components/MultistepForm";
+
+function App() {
+  return (
+    <div>
+      <MultistepForm />
+    </div>
+  );
+}
+
+export default App;
