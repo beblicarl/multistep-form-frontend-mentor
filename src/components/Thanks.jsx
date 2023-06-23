b20565bb11661c747f908d3cbe4e86d6f4ac4a0f
Carl Bebli <beblicarl.cb@gmail.com>
2023-06-23 08:56:59 +0000

    worked on typos

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -4,14 +4,14 @@ export default function Thanks() {
   return (
     <div className="text-center flex flex-col justify-center p-[1em] gap-y-4">
       <img
-        className="w-[3em] h-[5em] left-[25%] relative"
+        className="w-[3em] h-[5em] left-[45%] relative"
         src={thankYou}
         alt="thank-you"
       />
       <h1 className="text-MarineBlue font-bold text-2xl">Thank you!</h1>
       <p className="text-CoolGray font-semibold">
         Thanks for confirming your subscription! We hope you have fun using our
-        platfomr. If you ever need support, please feel free to email us at
+        platform. If you ever need support, please feel free to email us at
         support@loremgaming.com.
       </p>
     </div>
